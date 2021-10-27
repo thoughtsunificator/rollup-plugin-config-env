bad096e2fc3501be77bc0bb6f5d58f8f0f3dff70
thoughtsunificator <oss@thoughtsunificator.me>
2021-10-27 16:43:12 +0100

    updated deps

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs = require("fs")
+const { createConfig } = require("@thoughtsunificator/config-env")
 
 module.exports = config => {
 
@@ -22,39 +22,7 @@ module.exports = config => {
 		},
 		load ( id ) {
 			if(id === ":config") {
-				let envFile
-				if (fs.existsSync(envPath)) {
-					try {
-						let data = fs.readFileSync(envPath);
-						envFile = JSON.parse(data)
-					} catch(ex) {
-						console.error(ex)
-					}
-				} else {
-					envFile = {}
-				}
-				let configFile
-				if (fs.existsSync(configPath)) {
-					try {
-						let data = fs.readFileSync(configPath);
-						configFile = JSON.parse(data)
-					} catch(ex) {
-						console.error(ex)
-					}
-				} else {
-					configFile = {}
-				}
-				const config = {}
-				for(const key in configFile) {
-					config[key] = configFile[key]
-				}
-				for(const key in envFile) {
-					config[key] = envFile[key]
-				}
-				const processEnvKeys = Object.keys(config).filter(key => key in process.env)
-				for(const key of processEnvKeys) {
-					config[key] = process.env[key]
-				}
+				const config = createConfig(envPath, configPath)
 				return `export default ${JSON.stringify(config)}`
 			}
 			return null;
